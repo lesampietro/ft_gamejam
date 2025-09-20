f91c8ec5713d6462f7a24be934a6dfb40a9e80f7
Sabrina Ferreira Nascimento <sabrifer@student.42sp.org.br>
2025-09-19 22:51:07 -0300

    update screen view

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends Area2D
-@export var speed = 200
-var screen_size
+@export var speed = 1000
+var screen_size: Vector2 # o tipo do retorno do screen size. x, y
+@onready var col_shape = $CollisionShape2D
 
 func start(pos):
 	position = pos
@@ -15,12 +16,14 @@ func _on_body_entered(_body):
 	$CollisionShape2D.set_deferred("disabled", true)
 
 func _ready() -> void:
-	screen_size = get_viewport_rect()
+	screen_size = get_viewport_rect().size # add size pq retorna o tamanho da tela
+	print("LOG: screen_size: ", screen_size)
 	$PlayerSprite.play()
 	
 	
 func check_keyboard_actions() -> Vector2:
 	var velocity = Vector2.ZERO
+	print("LOG: velocity: ", velocity)
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -28,7 +31,7 @@ func check_keyboard_actions() -> Vector2:
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		velocity.y += 1	
 	return velocity
 
 func playerMove(delta: float) -> void: 
@@ -52,6 +55,10 @@ func playerMove(delta: float) -> void:
 
 	if movement.length() > 0:
 		position += movement.normalized() * speed * delta
+	position.x = clamp(position.x, + 40, screen_size.x - 45)
+	position.y = clamp(position.y, -125, screen_size.y - 225)
+	print("position.x: ", position.x)
+	print("position.y: ", position.y)
 
 func playerActions(delta: float) -> void:
 	playerMove(delta)
